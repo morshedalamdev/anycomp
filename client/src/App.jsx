715e8ad34d95805836a677faa99772fe6db321c6
Morshed Alam <ma.morshedalampro@gmail.com>
2025-12-09 22:14:30 +0800

    Add Proxima Nova fonts and update styles
    Added Proxima Nova font files and defined @font-face rules in index.css. Updated the App sidebar to include a 'Profile' heading and set the default body font to use Proxima Nova.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
     <aside className="flex flex-col">
       <div className="flex flex-col pt-[31px] left-[22px]">
-        
+        <h4 className="font-bold text-[11px] text-[#222222]">Profile</h4>
       </div>
     </aside>
     <main></main>
