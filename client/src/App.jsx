0d78641cd8c45077ac0ed868f4298e823a66cfa0
Morshed Alam <ma.morshedalampro@gmail.com>
2025-12-09 21:42:37 +0800

    Initialize React + Vite project structure
    Add base project files including .gitattributes, .gitignore, README, and client directory with React + Vite setup. Includes initial HTML, assets, configuration, and source files for starting development.

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+    <aside className="flex flex-col">
+      <div className="flex flex-col pt-[31px] left-[22px]">
+        
+      </div>
+    </aside>
+    <main></main>
+    </>
+  )
+}
+
+export default App
